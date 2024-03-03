ae5d8b09cbfbc641b07ae48eb7dc068a70852844
Lucas <luck.camara@gmail.com>
2024-03-02 23:14:01 -0300

    chore: add crud functions

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from models.servico import bp_servico
 from models.carro import bp_carro
 
 app = Flask(__name__)
+
+# Database
 connect =  "sqlite:///oficina.sqlite"
 app.config['SECRET_KEY'] = 'root'
 app.config['SQLALCHEMY_DATABASE_URI'] = connect
@@ -15,10 +17,16 @@ app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# Blueprints
+app.register_blueprint(bp_carro, url_prefix='/carro')
+app.register_blueprint(bp_cliente, url_prefix='/cliente')
+app.register_blueprint(bp_funcionario, url_prefix='/funcionario')
+app.register_blueprint(bp_servico, url_prefix='/servico')
+
+
 Bootstrap(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-app.run()
--- models/carro.py
+++ models/carro.py
@@ -8,7 +8,7 @@ bp_carro = Blueprint("carro", __name__, template_folder="templates")
 @bp_carro.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method=='GET':
-        return render_template('funcionario_create.html')
+        return render_template('carro_form.html')
 
     if request.method=='POST':
         marca = request.form.get('marca')
@@ -20,6 +20,40 @@ def create():
         db.session.add(query)
         db.session.commit()
 
-    return render_template('carro_create.html')
+    return render_template('carro_form.html')
+
+@bp_carro.route('/')
+def recovery():
+    carro = Carro.query.all()
+    return render_template('list.html', value=carro)
+
+@bp_carro.route('/update/<int:id>', methods=['GET', 'POST'])
 
     
+def update(id):
+    c = Carro.query.get(id)
+    if request.method=='GET':
+
+        return render_template('carro_form.html', value=c)
+    
+    if request.method=='POST':
+        marca = request.form.get('marca')
+        placa = request.form.get('placa')
+        modelo = request.form.get('modelo')
+        c.marca = marca
+        c.placa = placa
+        c.modelo = modelo
+        
+        db.session.add(c)
+        db.session.commit()
+
+@bp_carro.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+
+    c = Carro.query.get(id)
+
+    if request.method=='POST':
+        db.session.delete(c)
+        db.session.commit()
+
+
